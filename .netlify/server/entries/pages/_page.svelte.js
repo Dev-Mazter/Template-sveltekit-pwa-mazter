217ebf41fe74014fb8553d3bb849eaea3cfd5e0a
NelsonDJCR <dev@nelsondjcr.com>
2023-06-24 09:07:08 -0500

    delete folder .netlify

--- .netlify/server/entries/pages/_page.svelte.js
+++
@@ -1,9 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/index.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `${$$result.head += `<!-- HEAD_svelte-suuqfi_START -->${$$result.title = `<title>Title</title>`, ""}<meta name="${"description"}" content="${"Svelte demo app"}"><!-- HEAD_svelte-suuqfi_END -->`, ""}
-
-<section>Hello world!</section>`;
-});
-export {
-  Page as default
-};
