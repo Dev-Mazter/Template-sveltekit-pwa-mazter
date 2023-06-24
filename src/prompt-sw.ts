48eef28c47b80fe65a62aeae560aca6eef9295bc
NelsonDJCR <dev@nelsondjcr.com>
2023-06-24 00:59:54 -0500

    test

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -1,25 +1,22 @@
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import { NavigationRoute, registerRoute } from 'workbox-routing';
 
-declare let self: ServiceWorkerGlobalScope
+declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener('message', (event: { data: { type: string } }) => {
-  if (event.data && event.data.type === 'SKIP_WAITING')
-    self.skipWaiting()
-})
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
-// self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST || []);
 
-// clean old assets
-cleanupOutdatedCaches()
+cleanupOutdatedCaches();
 
-let allowlist: undefined | RegExp[]
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+const allowlist: RegExp[] | undefined = import.meta.env.DEV ? [/^\/$/] : undefined;
 
-// to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('/'),
-  { allowlist },
-))
+registerRoute(
+  new NavigationRoute(createHandlerBoundToURL('/'), {
+    allowlist,
+  })
+);
