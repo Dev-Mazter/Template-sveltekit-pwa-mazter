83b282a72e9eb86eb59b3f9e345591f346df7d65
NelsonDJCR <dev@nelsondjcr.com>
2023-06-24 01:24:06 -0500

    test

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -1,12 +1,17 @@
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching';
+import {
+	cleanupOutdatedCaches,
+	createHandlerBoundToURL,
+	precacheAndRoute
+} from 'workbox-precaching';
+import { NetworkFirst } from 'workbox-strategies';
 import { NavigationRoute, registerRoute } from 'workbox-routing';
 
 declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener('message', (event: { data: { type: string } }) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+	if (event.data && event.data.type === 'SKIP_WAITING') {
+		self.skipWaiting();
+	}
 });
 
 precacheAndRoute(self.__WB_MANIFEST || []);
@@ -15,8 +20,11 @@ cleanupOutdatedCaches();
 
 const allowlist: RegExp[] | undefined = import.meta.env.DEV ? [/^\/$/] : undefined;
 
-registerRoute(
-  new NavigationRoute(createHandlerBoundToURL('/xx'), {
-    allowlist,
-  })
-);
+registerRoute(({ url }) => url.pathname === '/', new NetworkFirst());
+
+// Initial
+// registerRoute(
+// 	new NavigationRoute(createHandlerBoundToURL('/'), {
+// 		allowlist
+// 	})
+// );
