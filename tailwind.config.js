ba8603e8c454458dd72e637e304cd9b6160f89b3
NelsonDJCR <dev@nelsondjcr.com>
2023-06-27 20:50:58 -0500

    add customs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,svelte}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'fit-contend': 'fit-content',
+      }
+    },
   },
   plugins: [],
 }
