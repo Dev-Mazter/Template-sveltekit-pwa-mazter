989c6dafd330ce0f7cf292def3b298c9b56f4481
NelsonDJCR <dev@nelsondjcr.com>
2023-07-16 15:46:54 -0500

    register: add new help

--- src/lib/helpers/helper.js
+++ src/lib/helpers/helper.js
@@ -4,3 +4,21 @@ import { myFunction } from "$lib/helpers/helper.js"; */
 export function myFunction() {
     return "siuuuuuuuuuu"
 }
+
+export function money(number = 0) {
+    return `$${number.toLocaleString('es-ES')}`;
+}
+export function changePage(url = '') {
+    let shopLink = document.querySelector(`a[href*="${url}"]`);
+    if (!shopLink) {
+        const link = document.createElement('a');
+        link.href = `${url}`;
+        link.style.width = '0.1px';
+        link.style.height = '0.1px';
+        const body = document.body;
+        body.appendChild(link);
+        shopLink = document.querySelector(`a[href*="${url}"]`);
+    } 
+    // @ts-ignore
+    shopLink.click();
+}
