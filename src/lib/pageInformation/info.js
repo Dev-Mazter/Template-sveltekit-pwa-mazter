15ed2366cf5a2e126ffd767dc8e34e64c9e4e0f0
NelsonDJCR <dev@nelsondjcr.com>
2023-08-25 11:31:50 -0500

    update

---
+++ src/lib/pageInformation/info.js
@@ -0,0 +1,19 @@
+export const INFO = {
+    APP_NAME:'',
+    PHONE:'',
+    PHONE_SEONDARY:'',
+    EMAIL:'',
+    EMAIL_SEONDARY:'',
+    ADDRESS:'',
+    COUNTRY:'',
+    CITY:'',
+    COUNTRY_CODE:'',
+}
+export const SOCIAL = {
+    FACEBOOK:'',
+    INSTAGRAM:'',
+    X:'',
+    TIKTOK:'',
+    TELEGRAM:'',
+    WHATSAPP:'',
+}
